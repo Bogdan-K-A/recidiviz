03dbf3d1ddd3f9941bf073b46aaeb4f9f24fea02
Bogdan-K-A <kozhoma_b@ukr.net>
2022-12-25 20:28:18 +0200

    fix

--- src/components/Hero/Hero.styled.js
+++ src/components/Hero/Hero.styled.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import heroImg from '../../assets/images/hero-img.png'
 
 export const StyledHero = styled.section`
